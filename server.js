c96eeb44fd66c0030cac2bbdcaab1a1181db17ce
Hrithin <100468394+hrithinnnn@users.noreply.github.com>
2023-05-28 15:59:26 +0530

    server changes

--- server.js
+++ server.js
@@ -31,12 +31,12 @@ mongoose.connect(URI, {
 
 
 // Below MongoDB and  Above Listen Sever
-if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'));
-    app.get('*', (req, res) =>{
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
-    });
-}
+// if(process.env.NODE_ENV === 'production'){
+//     app.use(express.static('client/build'));
+//     app.get('*', (req, res) =>{
+//         res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+//     });
+// }
 
 
 
